7fa738b7a1401e199ac7a57b1245ce81623b654b
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-12 00:16:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let ans="#";
+	for(let i=0;i<shortHex.length;i++){
+		if(shortHex[i]=='#')continue;
+		ans+=shortHex[i]+shortHex[i];
+	}
+	return ans;
 };
 
 // Do not change the code below.
